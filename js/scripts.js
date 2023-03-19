b373d623ed577ced532baa716af6be340f0ec7e7
Genesis Scott <chandlerscott123@gmail.com>
2023-03-19 13:48:42 -0500

    Fix sumCardDigits failing unit test for inputs>9 Failing unit test verifies the sum function correctly processes inputs of more than one char in length Test Cases: sumCardDigits(11) should return 2 sumCarddigits(1234) should return 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 
 
-function sumCardDigits(cardNum){
-  let sum=0; 
-  sum = parseInt(cardNum[0]);
-  return sum;
+function sumCardDigits(cardNumStr){
+  let sum=0;
+  let num;
+  let cardNumArr = cardNumStr.split(""); //make array
+  
+  //check length of cardNumArr
+  if(cardNumStr.length===1){
+    
+    //convert to int and return sum if length is 1
+    sum = parseInt(cardNumStr[0]);
+    return sum;
+  }
+
+  else if (cardNumStr.length>0){
+    //iterate through the array
+    cardNumArr.forEach(function(number){
+      num = parseInt(number); //change each char to int
+      sum += num; //add each num to sum
+    });
+    return sum;
+  }
 
 }
